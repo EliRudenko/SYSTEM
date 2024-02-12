fc7b69a0fb47c5a133a5f9117172da87edac6999
Eli Rudenko <rudenko.elin@gmail.com>
2024-02-13 00:03:04 +0200

    HW

--- Projects_HW/Controls/StaticAndButton/Main.cpp
+++ Projects_HW/Controls/StaticAndButton/Main.cpp
@@ -5,7 +5,7 @@ HINSTANCE hInstance;
 
 const int STATIC_WIDTH = 50;
 const int STATIC_HEIGHT = 50;
-const int MOVEMENT_SPEED = 2; // Медленная скорость перемещения
+const int MOVEMENT_SPEED = 2;
 
 HWND hStatic;
 RECT rcDialog;
@@ -18,31 +18,35 @@ void MoveStatic(HWND hWnd);
 
 INT_PTR CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-// Объявление нового пользовательского сообщения
+//cvc
 #define WM_MOVE_STATIC (WM_USER + 1)
 
-void MoveStatic(HWND hWnd) {
+void MoveStatic(HWND hWnd) 
+{
     // Получаем текущие координаты статика
     RECT rcStatic;
     GetWindowRect(hStatic, &rcStatic);
     ScreenToClient(GetParent(hStatic), reinterpret_cast<LPPOINT>(&rcStatic.left));
     ScreenToClient(GetParent(hStatic), reinterpret_cast<LPPOINT>(&rcStatic.right));
 
-    if (IsMouseOver(hStatic)) { // Инвертированное условие
-        // Вычисляем вектор движения
+    //ПО ИДЕЕ ТО ЧТО В ПРОШЛОМ ДЗ С ДВИЖЕНИЕМ БЫЛО
+    if (IsMouseOver(hStatic)) 
+    {
+    //ВЕКТОР ДВИЖЕНИЯ
         int deltaX = targetX - rcStatic.left;
         int deltaY = targetY - rcStatic.top;
 
-        // Рассчитываем новые координаты с учетом медленной скорости
+        //НОВЫЕ КООРДИНАТЫ И СКОРОСТЬ
         int newX = rcStatic.left + (deltaX > 0 ? min(MOVEMENT_SPEED, deltaX) : max(-MOVEMENT_SPEED, deltaX));
         int newY = rcStatic.top + (deltaY > 0 ? min(MOVEMENT_SPEED, deltaY) : max(-MOVEMENT_SPEED, deltaY));
 
-        // Перемещаем статик
+        //ДВИЖ
         SetWindowPos(hStatic, NULL, newX, newY, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
-
-        // Проверяем, достигли ли мы целевых координат
-        if (abs(newX - targetX) <= MOVEMENT_SPEED && abs(newY - targetY) <= MOVEMENT_SPEED) {
-            // Генерируем новые координаты для перемещения статика
+        
+        //ПРОВЕРОЧКИ
+        if (abs(newX - targetX) <= MOVEMENT_SPEED && abs(newY - targetY) <= MOVEMENT_SPEED) 
+        {
+            //НОВЫЕ КООРДИНАТЫ
             StartMoving(hWnd);
 
             // Отправляем пользовательское сообщение WM_MOVE_STATIC для обновления статика
@@ -51,13 +55,15 @@ void MoveStatic(HWND hWnd) {
     }
 }
 
-void StartMoving(HWND hWnd) {
-    // Генерируем новые координаты для перемещения статика
+void StartMoving(HWND hWnd) 
+{
+    //НОВЫЕ КООРДИНАТЫ
     targetX = rand() % (rcDialog.right - STATIC_WIDTH - rcDialog.left);
     targetY = rand() % (rcDialog.bottom - STATIC_HEIGHT - rcDialog.top);
 }
 
-bool IsMouseOver(HWND hWnd) {
+bool IsMouseOver(HWND hWnd) 
+{
     POINT cursorPos;
     GetCursorPos(&cursorPos);
     ScreenToClient(hWnd, &cursorPos);
@@ -68,14 +74,18 @@ bool IsMouseOver(HWND hWnd) {
     return PtInRect(&rc, cursorPos);
 }
 
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd) {
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd) 
+{
     ::hInstance = hInstance;
     return DialogBox(hInstance, MAKEINTRESOURCE(IDD_MAIN_DIALOG), NULL, DlgProc);
 }
 
-INT_PTR CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
-    case WM_INITDIALOG: {
+INT_PTR CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
+{
+    switch (uMsg) 
+    {
+    case WM_INITDIALOG: 
+    {
         hStatic = GetDlgItem(hwnd, IDC_PICTURE);
         GetClientRect(hwnd, &rcDialog);
 
@@ -90,27 +100,30 @@ INT_PTR CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
         break;
     }
 
-    case WM_TIMER: {
-        // Обработка таймера для медленного перемещения статика
+    case WM_TIMER: 
+    {
         MoveStatic(hwnd);
         break;
     }
 
-    case WM_MOVE_STATIC: {
-        // Обработка пользовательского сообщения для обновления статика
+    case WM_MOVE_STATIC: 
+    {
         MoveStatic(hwnd);
         break;
     }
 
-    case WM_COMMAND: {
-        if (LOWORD(wParam) == IDC_BUTTON1 && HIWORD(wParam) == BN_CLICKED) {
+    case WM_COMMAND: 
+    {
+        if (LOWORD(wParam) == IDC_BUTTON1 && HIWORD(wParam) == BN_CLICKED) 
+        {
             EnableWindow(GetDlgItem(hwnd, IDC_BUTTON2), FALSE);
             MessageBox(NULL, L"Clicked", L"", MB_OK);
         }
         break;
     }
 
-    case WM_CLOSE: {
+    case WM_CLOSE: 
+    {
         EndDialog(hwnd, 0);
         return TRUE;
     }
